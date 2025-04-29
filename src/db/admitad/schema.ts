3c31efc67d405c7205a4e84fd6327d54fa39a63d
spidy-verse <eliteanswers.in@gmail.com>
2025-04-29 21:43:38 +0530

    pw: admitad postback ts errors solved
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/db/admitad/schema.ts
+++ src/db/admitad/schema.ts
@@ -2,7 +2,7 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const admitAd = sqliteTable("admitad_postback", {
     id: int().primaryKey({ autoIncrement: true }),
-    offer_id: int(),
+    offer_id: text(),
     offer_name: text(),
     subid: text(),
     action: text(),
@@ -11,11 +11,11 @@ export const admitAd = sqliteTable("admitad_postback", {
     subid2: text(),
     subid3: text(),
     subid4: text(),
-    payment_sum: int(),
+    payment_sum: text(),
     payment_status: text(),
     reward_ready: text(),
-    order_id: int(),
-    order_sum: int(),
+    order_id: text(),
+    order_sum: text(),
     click_time: text(),
     time: text(),
     convertion_time: text(),
