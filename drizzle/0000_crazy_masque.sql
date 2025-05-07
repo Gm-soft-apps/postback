8ad8a141cd4cc2a405e79220f424d23164c5ef04
eliteanswers.in <eliteanswers.in@gmail.com>
2025-05-07 10:27:05 +0000

    idx & drizzle migrations generated
    Signed-off-by: eliteanswers.in <eliteanswers.in@gmail.com>

---
+++ drizzle/0000_crazy_masque.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `admitad_postback` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`offer_id` text,
+	`offer_name` text,
+	`subid` text,
+	`action` text,
+	`action_id` text,
+	`subid1` text,
+	`subid2` text,
+	`subid3` text,
+	`subid4` text,
+	`payment_sum` text,
+	`payment_status` text,
+	`reward_ready` text,
+	`order_id` text,
+	`order_sum` text,
+	`click_time` text,
+	`time` text,
+	`convertion_time` text,
+	`type` text
+);
+--> statement-breakpoint
+CREATE TABLE `mdeal_postback` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`offer_id` text,
+	`transaction_id` text,
+	`subid1` text,
+	`subid2` text,
+	`subid3` text,
+	`subid4` text,
+	`subid5` text,
+	`convertion_time` integer
+);
--- drizzle/0002_far_baron_zemo.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `admitad_postback` ALTER COLUMN "offer_id" TO "offer_id" text;--> statement-breakpoint
-ALTER TABLE `admitad_postback` ALTER COLUMN "payment_sum" TO "payment_sum" text;--> statement-breakpoint
-ALTER TABLE `admitad_postback` ALTER COLUMN "order_id" TO "order_id" text;--> statement-breakpoint
-ALTER TABLE `admitad_postback` ALTER COLUMN "order_sum" TO "order_sum" text;
